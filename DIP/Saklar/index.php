2cd819b137bd96ef16a6f7641b7e2f79d550ae3b
Dera0512 <witerwhait4@gmail.com>
2024-04-17 15:03:42 +0700

    saklar

---
+++ DIP/Saklar/index.php
@@ -0,0 +1,12 @@
+<?php 
+include_once "Lampu.php";
+include_once "KipasAngin.php";
+include_once "Saklar.php";
+
+$lampu = New Lampu();
+$lampu->beroprasi();
+$lampu->berhenti();
+
+$Kipasangin = new KipasAngin();
+$Kipasangin->beroprasi();
+$Kipasangin->berhenti();
